446517cacf5ae61288d12d96f0ff46cf1bf49e45
Shpat Hashani <53412278+shpati2005@users.noreply.github.com>
2026-04-17 18:40:11 +0200

    v1.0.4
    Added InventoryItem.js, InventoryTransaction.js, InvoiceItem.js, Payment.js, Reminder.js and WorkSchedule.js

---
+++ models/Reminder.js
@@ -0,0 +1,32 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Reminder = sequelize.define('Reminder', {
+  reminder_id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  type: {
+    type: DataTypes.STRING(50),
+    allowNull: false
+  },
+  sent_date: {
+    type: DataTypes.DATE,
+    allowNull: true
+  },
+  status: {
+    type: DataTypes.STRING(20),
+    allowNull: false,
+    defaultValue: 'Pending'
+  },
+  appointment_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  }
+}, {
+  tableName: 'reminders',
+  timestamps: false
+});
+
+module.exports = Reminder;
